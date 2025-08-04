cd7b4973164b36e690ebd8b0a3b4e49b89b8ce9b
Patchara.DEV <patchara.pn31@gmail.com>
2025-08-04 16:38:51 +0700

    Fixed Some Bug

--- src/app/device/[specId]/page.tsx
+++ src/app/device/[specId]/page.tsx
@@ -141,7 +141,7 @@ export default function DevicePage() {
       style={{ backgroundImage: 'url("/bg.png")' }}
       className="flex justify-center font-sans items-center  min-h-screen p-8 pb-20 gap-4 sm:p-20 "
     >
-      <div className="p-10 w-90 h-fit justify-center items-center border border-blue-400 rounded-xl shadow-2xl">
+      <div className="p-10 w-90 h-fit bg-white justify-center items-center border border-blue-400 rounded-xl shadow-2xl">
         <h2 className="text-xl font-semibold mb-10">ID: {params.specId}</h2>{" "}
         <div className="flex gap-5 mb-15">
           <div>
